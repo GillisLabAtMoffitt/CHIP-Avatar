c54d4a580a5dd5e299eb5ef2fcf05d545c0ebd67
chrisleitzinger <christelleleitzinger@gmail.com>
2020-12-15 10:11:44 -0500

    OS by demo

--- R/7.survivals.R
+++ R/7.survivals.R
@@ -281,7 +281,7 @@ a <- summary(myplot)$table
 write.csv(a, paste0(path, "/summary PFS CHIP status from drug date.csv"))
 
 
-################################################################################### IV ### PFS drug date by demo----
+################################################################################### IV ### PFS/OS drug date by demo----
 mysurv <- Surv(time = germline_patient_data$month_at_progression_drug, event = germline_patient_data$progression_drug_surv)
 myplot <- survfit(mysurv~Ethnicity, data = germline_patient_data)
 jpeg(paste0(path, "/Output Survivals/PFS Ethnicity from drugs date.jpeg"), width = 1200, height = 900)
@@ -352,6 +352,79 @@ ggsurvplot(myplot, data = germline_race,
                                           font.y = c(16, "bold", "transparent"),
                                           font.tickslab = c(19, "bold", "black")
            ),
+           # Censor
+           censor = TRUE
+)
+dev.off()
+# OS
+mysurv <- Surv(time = germline_patient_data$month_at_os, event = germline_patient_data$os_surv_cor)
+myplot <- survfit(mysurv~Ethnicity, data = germline_patient_data)
+jpeg(paste0(path, "/Output Survivals/OS Ethnicity from drugs date.jpeg"), width = 1200, height = 900)
+ggsurvplot(myplot, data = germline_patient_data,
+           title = "OS from date of diagnosis",
+           font.main = c(24, "bold", "black"),
+           font.x = c(20, "bold", "black"),
+           font.y = c(20, "bold", "black"),
+           font.legend = c(20, "bold", "black"),
+           font.tickslab = c(18, "bold", "black"),
+           size = 1.5,
+           
+           xlab = "Time in months", 
+           legend = "top",
+           legend.title = "Ethnicity",
+           legend.labs = c("Hipanic", "Non-Hispanic", "Unknown"),
+           palette = c("darkred", "darkgreen", "grey"),
+           pval = TRUE,
+           conf.int = FALSE,
+           # Add risk table
+           tables.height = 0.3,
+           risk.table.title = "Risk table (number(%))",
+           risk.table = "abs_pct",
+           risk.table.y.text = FALSE,
+           risk.table.fontsize = 6,
+           tables.theme = theme_survminer(base_size = 5,
+                                          font.main = c(16, "bold", "black"),
+                                          font.x = c(16, "bold", "black"),
+                                          font.y = c(16, "bold", "transparent"),
+                                          font.tickslab = c(19, "bold", "black")),
+           # Censor
+           censor = TRUE
+)
+dev.off()
+
+germline_race <- germline_patient_data %>% 
+  mutate(Race = factor(Race, levels=c("White", "Black",  "Others"))) %>% filter(!is.na(Race))
+
+mysurv <- Surv(time = germline_race$month_at_os, event = germline_race$os_surv_cor)
+myplot <- survfit(mysurv~Race, data = germline_race)
+jpeg(paste0(path, "/Output Survivals/OS Race from drugs date.jpeg"), width = 1200, height = 900)
+ggsurvplot(myplot, data = germline_race,
+           title = "OS from date of diagnosis",
+           font.main = c(24, "bold", "black"),
+           font.x = c(20, "bold", "black"),
+           font.y = c(20, "bold", "black"),
+           font.legend = c(20, "bold", "black"),
+           font.tickslab = c(18, "bold", "black"),
+           size = 1.5,
+           
+           xlab = "Time in months", 
+           legend = "top",
+           legend.title = "Race",
+           legend.labs = c("White", "Black", "Unknown"),
+           palette = c("#A92E5EFF", "#E65D2FFF", "grey"),
+           pval = TRUE,
+           conf.int = FALSE,
+           # Add risk table
+           tables.height = 0.3,
+           risk.table.title = "Risk table (number(%))",
+           risk.table = "abs_pct",
+           risk.table.y.text = FALSE,
+           risk.table.fontsize = 6,
+           tables.theme = theme_survminer(base_size = 5,
+                                          font.main = c(16, "bold", "black"),
+                                          font.x = c(16, "bold", "black"),
+                                          font.y = c(16, "bold", "transparent"),
+                                          font.tickslab = c(19, "bold", "black")),
            # Censor
            censor = TRUE
 )

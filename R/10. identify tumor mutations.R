6353356fd5464154d3cae70143eef22a4c6d93d3
Christelle Leitzinger <55062754+chrisleitzinger@users.noreply.github.com>
2021-07-20 11:27:38 -0400

    Update 10. identify tumor mutations.R
    create list of mutated gene found in literature

--- R/10. identify tumor mutations.R
+++ R/10. identify tumor mutations.R
@@ -24,7 +24,7 @@ Hoang_2018 <- data.frame(
                                                  "FTL","OR9G1","TNFSF12","TRAF2",
                                                  "FAM154B","HIST1H4H","LEMD2",
                                                  "PABPC1","RPN1","SGPP1"),
-                `Walker.et.al.%.(n=463)` = c("21*",
+                `Walker_2015_n463` = c("21*",
                                                  "19*","6*","7*","3*","9*","2",
                                                  "2","4*","4*","3","2*",
                                                  "3*","3*","3*","3*","2","5",
@@ -34,7 +34,7 @@ Hoang_2018 <- data.frame(
                                                  "<1","<1","0","<1","0",
                                                  "<1","0","1","<1","1","<1",
                                                  "1","0","<1"),
-                   `Lohr.et.al.%(n=203)` = c("23*",
+                   `Lohr_2014_n203` = c("23*",
                                                  "20*","11*","6*","8*","11*",
                                                  "5*","4","4","5*","4","3",
                                                  "0","1","2","2","3*","2*",
@@ -44,7 +44,7 @@ Hoang_2018 <- data.frame(
                                                  "0","0","<1","0","0","<1",
                                                  "<1","2","<1","<1","0","1",
                                                  "0","1"),
-                   `Bolli.et.al.%(n=67)` = c("25*",
+                   `Bolli_2014_n67` = c("25*",
                                                  "25*","12*","15*","15*","1",
                                                  "0","7*","7","3","3","4",
                                                  "0","4*","0","0","0","0",
@@ -54,7 +54,7 @@ Hoang_2018 <- data.frame(
                                                  "0","0","0","0","0","0",
                                                  "0","0","0","0","0","0",
                                                  "0"),
-                       `CoMMpass.%(n=804)` = c("24*",
+                       `Hoang_2018_CoMMpass_n804` = c("24*",
                                                  "22*","9","7*","5*","10*","2",
                                                  "3*","4","7*","3","2",
                                                  "4*","3","3","3","2*","3",
@@ -65,35 +65,13 @@ Hoang_2018 <- data.frame(
                                                  "<1*","<1*","<1*","<1*","<1*",
                                                  "2*","<1*","<1*","<1*","4*",
                                                  "<1*","1*")
-              )
+              ) %>% 
+  mutate(Walker_2015_n463 =  str_replace(Walker_2015_n463, "\\*", "")) %>% 
+  mutate(Lohr_2014_n203 =  str_replace(Lohr_2014_n203, "\\*", "")) %>% 
+  
+  mutate(Bolli_2014_n67 =  str_replace(Bolli_2014_n67, "\\*", "")) %>% 
+  mutate(Hoang_2018_CoMMpass_n804 =  str_replace(Hoang_2018_CoMMpass_n804, "\\*", ""))
 
-Liu_2015 <- data.frame(
-                                 stringsAsFactors = FALSE,
-                                      check.names = FALSE,
-              `Gene Group Samples_with_mutation_in_group_(%)_n=648` = c("HIST1H1E All 2.8",
-                                                                  "IRF4 All 3.2","KRAS All 21.1",
-                                                                  "MAX All 2.4",
-                                                                  "NRAS All 19.4",
-                                                                  "TP53 All 3.0",
-                                                                  "TRAF3 All 3.7",
-                                                                  "FAM46C All 5.6","DIS3 All 8.6",
-                                                                  "BRAF All 6.7",
-                                                                  "LTB All 3.0",
-                                                                  "CYLD All 2.4",
-                                                                  "RB1 All 1.5",
-                                                                  "DIS3 t(4;14) 25.4",
-                                                                  "FGFR3 t(4;14) 16.9",
-                                                                  "KRAS t(11;14) 33.7",
-                                                                  "NRAS t(11;14) 25.6",
-                                                                  "DIS3 t(11;14) 11.6",
-                                                                  "IRF4 t(11;14) 10.5",
-                                                                  "KRAS HRD 20.6",
-                                                                  "NRAS HRD 24.4",
-                                                                  "FAM46C HRD 7.1","BRAF HRD 6.7",
-                                                                  "EGR1 HRD 4.6",
-                                                                  "CYLD HRD 2.9")
-            ) %>% 
-  separate(col = 'Gene Group Samples_with_mutation_in_group_(%)_n=648', into = c('Gene', 'Group_liu', 'Liu_2015'), sep = " ")
 
 Hollein_2020 <- data.frame(
   stringsAsFactors = FALSE,
@@ -104,6 +82,7 @@ Hollein_2020 <- data.frame(
   separate(col = 'n=211', into = c('Gene', 'Hollein_2020_n211'), sep = " ") %>% 
   mutate(Hollein_2020_n211 = str_replace_all(Hollein_2020_n211, "\\(|\\)|%", ""))
 
+
 Chapman_2011 <- data.frame(
   stringsAsFactors = FALSE,
        check.names = FALSE,
@@ -111,12 +90,12 @@ Chapman_2011 <- data.frame(
                                        "DISC3 4","TP53 3","CCND1 2","PNRC1 2",
                                        "ALOX12B 3","HLA-A 2","MAGED1 2")
                 ) %>% 
-  separate(col = 'n=38', into = c('Gene', 'Hollein_2020_n38'), sep = " ") %>% 
-  mutate(Hollein_2020_n38 = round((as.numeric(Hollein_2020_n38) /38) * 100, 2))
+  separate(col = 'n=38', into = c('Gene', 'Chapman_2011_n38'), sep = " ") %>% 
+  mutate(Chapman_2011_n38 = round((as.numeric(Chapman_2011_n38) /38) * 100, 2))
 
 
 bolli_2018 <-
-  readxl::read_xlsx(paste0(path, "/TumorMuts/litterature/Nature Bolli 2018.xlsx")) %>% 
+  readxl::read_xlsx(paste0(path, "/TumorMuts/litterature/Bolli Nature 2018.xlsx")) %>% 
   filter((Variant_class == "Oncogenic")) %>% 
   distinct(Sample, Gene) %>% 
   mutate(total = NROW(unique(Sample))) %>% 
@@ -184,9 +163,13 @@ Manojlovi_2017 <- data.frame(
 Somatic_tumor_mutation <- full_join(bolli_2018, Chapman_2011, by = "Gene") %>% 
   full_join(., Hoang_2018, by = "Gene") %>% 
   full_join(., Hollein_2020, by = "Gene") %>% 
-  full_join(., Liu_2015, by = "Gene") %>% 
+  # full_join(., Walker_2015, by = "Gene") %>% 
   full_join(., Manojlovi_2017, by = "Gene") %>% 
-  arrange(desc(bolli_2018_n342))
+  mutate_at(c( "Lohr_2014_n203", "Walker_2015_n463", "Bolli_2014_n67", 
+               "Hoang_2018_CoMMpass_n804" , "Hollein_2020_n211", "Manojlovi_2017_Caucasian_n591", "Manojlovi_2017_AfricanAmerican_n127" ), as.numeric) %>% 
+  mutate(average_percent = round(rowMeans(select(., Lohr_2014_n203: Manojlovi_2017_Caucasian_n591), na.rm = TRUE), 2) ) %>% 
+  arrange(desc(average_percent)) %>% 
+  select(Gene, average_percent, everything())
   
 write_csv(Somatic_tumor_mutation, paste0(path, "/TumorMuts/litterature/Somatic_tumor_mutation.csv"))
 
@@ -216,37 +199,23 @@ write_csv(Somatic_tumor_mutation, paste0(path, "/TumorMuts/litterature/Somatic_t
 #   mutate(cancercell_lohr=(count/203*100)) %>% 
 #   select(Gene = gene, cancercell_lohr)
 
-# jco_walker <-
-#   data.frame(data=matrix(c("Overall",
-#                       "HIST1H1E All 2.8 15 1 < 9×10 < 9×10",
-#                       "IRF4 All 3.2 15 1 < 9×10 < 9×10",
-#                       "KRAS All 21.1 104 0 < 9×10 < 9×10","MAX All 2.4 13 0 < 9×10 < 9×10",
-#                       "NRAS All 19.4 91 0 < 9×10 < 9×10",
-#                       "TP53 All 3.0 16 0 < 9×10 < 9×10","TRAF3 All 3.7 19 0 < 9×10 < 9×10",
-#                       "FAM46C All 5.6 26 1 5.00 ×10 1.05 ×10",
-#                       "DIS3 All 8.6 48 1 5.88 ×10 1.11 ×10","BRAF All 6.7 36 0 8.77 ×10 1.50 ×10",
-#                       "LTB All 3.0 14 1 2.84 ×10 4.46 ×10",
-#                       "CYLD All 2.4 14 0 6.65 ×10 9.66 ×10",
-#                       "RB1 All 1.5 7 0 1.96 ×10 2.64 ×10",
-#                       "t(4;14)",
-#                       "DIS3 t(4;14) 25.4 18 0 7.05 ×10 1.33 ×10","FGFR3 t(4;14) 16.9 12 0 6.93 ×10 6.54 ×10",
-#                       "t(11;14)",
-#                       "KRAS t(11;14) 33.7 31 0 < 9×10 < 9×10",
-#                       "NRAS t(11;14) 25.6 22 0 1.44 ×10 1.36 ×10",
-#                       "DIS3 t(11;14) 11.6 12 0 5.74 ×10 3.61 ×10",
-#                       "IRF4 t(11;14) 10.5 9 0 1.18 ×10 5.57 ×10",
-#                       "Hyperdiploidy",
-#                       "KRAS HRD 20.6 54 0 < 9×10 < 9×10","NRAS HRD 24.4 53 0 < 9×10 < 9×10",
-#                       "FAM46C HRD 7.1 15 1 6.99 ×10 4.40 ×10",
-#                       "BRAF HRD 6.7 16 0 1.45 ×10 6.85 ×10",
-#                       "EGR1 HRD 4.6 11 1 4.65 ×10 1.70 ×10","CYLD HRD 2.9 11 0 1.89 ×10 5.09 ×10"), ncol = 1)
-#   ) %>% 
-#   mutate(data = str_replace(data, "< ", "<")) %>% 
-#   separate(col = data, into = c("Gene", "Group", "Samples with mutation in group (%)","Nonsynonymous Mutations (n=)","Synonymous Mutations (n=)", "p-value q-value"), sep = " ") %>% 
-#   filter(Group == "All") %>% 
-#   select(Gene, jco_walker = "Samples with mutation in group (%)")
-
-
+# Walker_2015 <- data.frame(
+#                                  stringsAsFactors = FALSE,
+#                                       check.names = FALSE,
+#               `Gene Group Samples_with_mutation_in_group_(%)_n=648` = c("HIST1H1E All 2.8",
+#                                                                   "IRF4 All 3.2","KRAS All 21.1",
+#                                                                   "MAX All 2.4",
+#                                                                   "NRAS All 19.4",
+#                                                                   "TP53 All 3.0",
+#                                                                   "TRAF3 All 3.7",
+#                                                                   "FAM46C All 5.6","DIS3 All 8.6",
+#                                                                   "BRAF All 6.7",
+#                                                                   "LTB All 3.0",
+#                                                                   "CYLD All 2.4",
+#                                                                   "RB1 All 1.5")
+#             ) %>% 
+#   separate(col = 'Gene Group Samples_with_mutation_in_group_(%)_n=648', into = c('Gene', 'Group_Walker_2015', 'Walker_2015'), sep = " ") %>% 
+#   select(-Group_Walker_2015)
   
   
   
